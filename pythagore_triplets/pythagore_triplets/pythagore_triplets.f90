10adb2ac3c63a95662b04c663939bc9047508047
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-11-19 08:31:42 +0000

    Adds project and solution files.

---
+++ pythagore_triplets/pythagore_triplets/pythagore_triplets.f90
@@ -0,0 +1,26 @@
+!  pythagore_triplets.f90 
+!
+!  FUNCTIONS:
+!  pythagore_triplets - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: pythagore_triplets
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program pythagore_triplets
+
+    implicit none
+
+    ! Variables
+    
+
+    ! Body of pythagore_triplets
+    print *, 'Hello World'
+
+    end program pythagore_triplets
+
